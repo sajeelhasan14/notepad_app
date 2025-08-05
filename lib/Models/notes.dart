dc1ba114b848e689a9670159411dd874e903730c
sajeelhasan14 <sajeel.hasan14@gmail.com>
2025-08-06 00:42:45 +0500

    Update notes.dart

--- lib/Models/notes.dart
+++ lib/Models/notes.dart
@@ -4,12 +4,10 @@ class Notes {
 
   Notes({required this.title, required this.description});
 
-  // Convert Note object → Map<String, String>
   Map<String, String> toMap() {
     return {'title': title, 'description': description};
   }
 
-  // Convert Map<String, String> → Note object
   factory Notes.fromMap(Map<String, dynamic> map) {
     return Notes(title: map['title'], description: map['description']);
   }
